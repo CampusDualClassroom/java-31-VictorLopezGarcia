51a88d0a4c89e8d9a89eec1a9c9d4a121294a987
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-23 09:43:22 +0200

    Ejercicio31

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,20 @@
 package com.campusdual.classroom;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
-
+        //Leer txt e imprimir por pantalla
+        File file = new File("src/main/resources/lorem.txt");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
